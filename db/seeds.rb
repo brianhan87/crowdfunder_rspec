738ce007a47052d6afa26c055098b664d444b16f
Brian Han <brianhan87usa@gmail.com>
2013-09-30 15:22:11 -0400

    Seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+Project.destroy_all
+
+user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.safe_email, password: "password")
+
+50.times do |i|
+  project1 = user.projects.create!(title: "Project #{i}", teaser: Faker::Company.bs,
+    description: "description #{i}", goal: 13000)
+end
