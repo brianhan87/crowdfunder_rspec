56e55c215c9d84e9e5aaf341cc84f89ac1850ebf
Brian Han <brianhan87usa@gmail.com>
2013-10-01 23:59:54 -0400

    Writing tests for login and logout

--- spec/features/user_authentication_flows_spec.rb
+++ spec/features/user_authentication_flows_spec.rb
@@ -37,5 +37,30 @@ describe "UserAuthenticationFlows" do
   			have_content("Try again")
   		end
   	end
+
+  	it "should successfully log in" do
+  		visit '/'
+  		find('.navbar').has_no_link?('Logout').should be_true
+  		user = setup_signed_in_user
+  		find('.navbar').has_link?('Logout').should be_true
+  	end
+
+  	it "should unsuccessfully log in" do 
+  		visit '/sessions/new'
+
+  		fill_in "email", :with => "a@b.com"
+  		fill_in "password", :with => "invalid creds"
+  		click_button "Login"
+  	end
+
+  	it "should successfully logout" do 
+  		user = setup_signed_in_user
+
+  		visit '/'
+  		find('.navbar').click_link 'Logout'
+  		expect(page).to have_content("Bye")
+
+  		find('.navbar').has_no_link?('Logout')
+  	end
   end
 end
