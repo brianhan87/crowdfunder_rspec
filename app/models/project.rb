3776c6d0074cd63f225ab0babd299c6d87fa4a30
Brian Han <brianhan87usa@gmail.com>
2013-10-03 13:23:40 -0400

    Generate Pledge model and associations; Remove empty specs

--- app/models/project.rb
+++ app/models/project.rb
@@ -14,6 +14,7 @@
 
 class Project < ActiveRecord::Base
 	belongs_to :user
+	has_many :pledges
   attr_accessible :description, :goal, :teaser, :title
 
   validates :title, presence: true, length: { maximum: 100 }
--- spec/features/user_authentication_flows_spec.rb
+++ spec/features/user_authentication_flows_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "UserAuthenticationFlows" do
   describe "when visiting sign in page" do
-  	user = FactoryGirl.build(:user)
+  	let!(:user) { FactoryGirl.build(:user) }
   	it "should successfully register a user" do 
   		visit "/users/new"
       expect(current_path).to eq(new_user_path)
@@ -11,8 +11,6 @@ describe "UserAuthenticationFlows" do
       within(:css, '.navbar') do
         find_link("Sign Up").visible?
       end
-
-      user = FactoryGirl.build(:user) 
 
       # Fill in the from with the info from user created by FactoryGirl
       fill_in "user[email]", :with => user.email
@@ -32,6 +30,23 @@ describe "UserAuthenticationFlows" do
         has_link?('Logout')
       end
   	end
+
+    it "should fail registration" do 
+      visit "/users/new"
+
+      # Invalid form submission ...
+      fill_in "user[email]", :with => user.email
+      click_button "Create Account"
+      # Should redirect to users_path and ...
+      expect(current_path).to eq(users_path)
+      # ... no message saying "Account created" should appear
+
+      # page.should have_no_content("Account created")
+      expect(page).to have_no_content("Account created")
+      within(:css, ".alert") do
+        have_content("Try again")
+      end
+    end
 
   	it "should successfully log in" do
   		visit '/'
